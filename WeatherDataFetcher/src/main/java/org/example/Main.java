6d927696451bcece0fb9821727736bf38093a371
Vettas Alexandros <alexandros.vettas@gmail.com>
2025-06-18 19:41:48 +0300

    corrected MAIN

--- WeatherDataFetcher/src/main/java/org/example/Main.java
+++ WeatherDataFetcher/src/main/java/org/example/Main.java
@@ -8,12 +8,14 @@ public class Main {
     public static void main(String[] args) {
         
         port(8080); 
+
         // CORS setup 
         before((request, response) -> {
             response.header("Access-Control-Allow-Origin", "*");
             response.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
             response.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
         });
+
         // Handle preflight 
         options("/*", (request, response) -> {
             response.status(200);
